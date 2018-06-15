08a3baf80165a8eb7fe17ee166ddd5e600667b0c
Aaron Harris <aaron@itprousa.com>
2018-06-15 15:58:00 -0500

    Flesh out app interface
    Except for the complete lack of behavior, this is the entire interface
    for the app.

--- itpro-standard/src/main/java/com/us/itp/sandbox/webarch/controller/MainController.java
+++ itpro-standard/src/main/java/com/us/itp/sandbox/webarch/controller/MainController.java
@@ -1,13 +1,16 @@
 package com.us.itp.sandbox.webarch.controller;
 
+import java.util.Arrays;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    public String mainPage() {
+    public String mainPage(Model model) {
+        model.addAttribute("words", Arrays.asList("Foo", "Bar"));
         return "main";
     }
 }

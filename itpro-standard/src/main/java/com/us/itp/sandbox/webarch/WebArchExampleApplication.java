cf75cc451caa719bbae5b4589766413ff4e75f63
Aaron Harris <aaron@itprousa.com>
2018-06-15 12:06:38 -0500

    Set up ITProFound Standard module
    This module will use the default ITProFound Web App tech stack:
    * Maven
    * Java
    * Spring
    * ThymeLeaf
    * JavaScript
    * JQuery
    
    It should be familiar to most ITProFound developers and will serve as a
    baseline to help them understand the other modules in this repository.

---
+++ itpro-standard/src/main/java/com/us/itp/sandbox/webarch/WebArchExampleApplication.java
@@ -0,0 +1,12 @@
+package com.us.itp.sandbox.webarch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebArchExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebArchExampleApplication.class, args);
+    }
+}

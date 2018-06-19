d6cef25556c501545a5353edc2dd387d6d264284
Aaron Harris <aaron@itprousa.com>
2018-06-19 11:02:56 -0500

    Implement simplistic word list persistence service.
    Once this is integrated with the controller, the user will be able to
    add new words to the list and see them reflected in the app.
    
    In a real web app, this would be backed by a database; our very simple
    implementation is instead backed by a list.
    
    Also liberalize style rules to allow for adjacent declarations without
    intervening blank lines (for use especially in interface declarations).

---
+++ itpro-standard/src/main/java/com/us/itp/sandbox/webarch/service/WordServiceImpl.java
@@ -0,0 +1,35 @@
+package com.us.itp.sandbox.webarch.service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Service;
+
+@Service
+public final class WordServiceImpl implements WordService {
+
+    public WordServiceImpl() {
+        this(Collections.emptyList());
+    }
+
+    public WordServiceImpl(@NonNull String... words) {
+        this(Arrays.asList(words));
+    }
+
+    public WordServiceImpl(@NonNull List<String> words) {
+        this.words.addAll(words);
+    }
+
+    // In a real web app, this would be a data repository bean.
+    @NonNull private final List<String> words = new ArrayList<>();
+
+    @NonNull @Override public List<String> listAllWords() {
+        return Collections.unmodifiableList(words);
+    }
+
+    @Override public void addWord(@NonNull String word) {
+        words.add(word);
+    }
+}
---
+++ itpro-standard/src/test/java/com/us/itp/sandbox/webarch/service/WordServiceTests.java
@@ -0,0 +1,68 @@
+package com.us.itp.sandbox.webarch.service;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.core.IsCollectionContaining.hasItem;
+import static org.junit.Assert.assertThat;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.springframework.lang.NonNull;
+
+public final class WordServiceTests {
+
+    @NonNull @Rule public final ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void wordListIsInitializedFromConstructor() {
+        wordListIsInitializedFromConstructor(Collections.emptyList());
+        wordListIsInitializedFromConstructor("Alpha", "Bravo");
+    }
+
+    private void wordListIsInitializedFromConstructor(String... words) {
+        wordListIsInitializedFromConstructor(Arrays.asList(words));
+    }
+
+    private void wordListIsInitializedFromConstructor(List<String> words) {
+        final WordService service = new WordServiceImpl(words);
+        assertThat(service.listAllWords(), is(words));
+    }
+
+    @Test
+    public void addedWordAppearsInService() {
+        final WordService service = new WordServiceImpl("Foo");
+        final String word = "Bar";
+        service.addWord(word);
+        assertThat(service.listAllWords(), hasItem(word));
+    }
+
+    @Test
+    public void wordListIsInInsertionOrder() {
+        final List<String> words = Arrays.asList("Foo", "Bar", "Baz", "Quux");
+        final WordService service = new WordServiceImpl();
+        for (String word : words) {
+            service.addWord(word);
+        }
+        assertThat(service.listAllWords(), is(words));
+    }
+
+    @Test
+    public void wordListIsImmutable() {
+        final WordService service = new WordServiceImpl("Foo");
+        final List<String> words = service.listAllWords();
+        thrown.expect(UnsupportedOperationException.class);
+        words.add("Bar");
+    }
+
+    @Test
+    public void wordListTracksChanges() {
+        final WordService service = new WordServiceImpl("Foo, Bar");
+        final List<String> words = service.listAllWords();
+        final String newWord = "Baz";
+        service.addWord(newWord);
+        assertThat(words, hasItem(newWord));
+    }
+}
